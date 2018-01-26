1ea41ca6604aa8689d85e033424392e42ffc1760
Sean Oughton <seanoughton@gmail.com>
2018-01-26 17:26:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if (array.size!=0) && (array.size>2)
+    last_item = array[-1]
+    array.pop()
+    new_string = array.join(", ")
+    new_string << ", and " + last_item
+  elsif array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  end
 end
